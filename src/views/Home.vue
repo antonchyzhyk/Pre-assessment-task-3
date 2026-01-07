c96de4f4ad41fd25a9856fdfd0a622190697b183
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-07 22:07:26 +0200

    feature: colors setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <h1 class="text-primary">Home</h1>
+    <h1 class="text-red-500">Home</h1>
+    <p class="text-brand-800">brand-800</p>
+    <p class="text-secondary-600">secondary-600</p>
+    <div class="text-black">black</div>
     <el-button type="primary">Click me</el-button>
+    <el-button plain>Click me</el-button>
   </div>
 </template>
 
--- tailwind/tailwind.colors.ts
+++ tailwind/tailwind.colors.ts
@@ -2,6 +2,7 @@ import twColors from 'tailwindcss/colors'
 
 export const colors = {
   white: twColors.white,
+  black: '#000000',
   gray: twColors.gray,
   red: twColors.red,
   green: twColors.green,
@@ -11,5 +12,17 @@ export const colors = {
   primary: 'var(--el-color-primary)',
   success: 'var(--el-color-success)',
   warning: 'var(--el-color-warning)',
-  danger: 'var(--el-color-danger)'
+  danger: 'var(--el-color-danger)',
+
+  'brand-500': '#51585F',
+  'brand-800': '#21262C',
+  'brand-900': '#191D22',
+
+  'neutral-100': '#F4F8FB',
+  'neutral-400': '#E3EEF6',
+
+  'slate-gray-100': '#C5C7CA33',
+  'slate-gray-200': '#93989C',
+
+  'secondary-600': '#34739D'
 }
