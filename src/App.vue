df2d75b94ff4c784502abba33e7cf38724b62a12
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-07 21:33:44 +0200

    initial scaffold setup

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <ElConfigProvider>
+    <router-view />
+    <Modals />
+  </ElConfigProvider>
+</template>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-primary">Home</h1>
+    <el-button type="primary">Click me</el-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
