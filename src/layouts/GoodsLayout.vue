e6919630dd8cf92cc26698ad1ce4c955a46c0025
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 18:27:24 +0200

    feature: >layout - center content with max width

--- src/layouts/GoodsLayout.vue
+++ src/layouts/GoodsLayout.vue
@@ -1,12 +1,14 @@
 <template>
-  <header
-    class="py-[12px] px-[16px] lg:pt-[74px] lg:px-[74px] lg:pb-[24px] bg-white"
-  >
-    <BreadcrumbHeader />
-  </header>
-  <main class="px-[16px] pb-[16px] lg:px-[74px] lg:pb-[74px] h-full">
-    <router-view />
-  </main>
+  <div class="max-w-[1440px] mx-auto">
+    <header
+      class="py-[12px] px-[16px] lg:pt-[74px] lg:px-[74px] lg:pb-[24px] bg-white"
+    >
+      <BreadcrumbHeader />
+    </header>
+    <main class="px-[16px] pb-[16px] lg:px-[74px] lg:pb-[74px] h-full">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script lang="ts" setup>
