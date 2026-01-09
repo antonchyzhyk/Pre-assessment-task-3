6557dc5b6da54f0363466d4e30794fa48ff0ab48
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 23:38:48 +0200

    minor-refactor

--- src/components/BreadcrumbHeader.vue
+++ src/components/BreadcrumbHeader.vue
@@ -7,19 +7,19 @@
         class="flex items-center"
       >
         <router-link
-          v-if="index < breadcrumbItems.length - 1"
           :to="item.path"
           class="text-slate-gray-200 hover:text-secondary-600 underline"
+          :class="{ '!text-secondary-600 no-underline font-semibold': index === breadcrumbItems.length - 1 }"
         >
           {{ item.label }}
         </router-link>
 
-        <span
+        <!-- <span
           v-else
           class="text-secondary-600 font-semibold"
         >
           {{ item.label }}
-        </span>
+        </span> -->
 
         <span
           v-if="index < breadcrumbItems.length - 1"
