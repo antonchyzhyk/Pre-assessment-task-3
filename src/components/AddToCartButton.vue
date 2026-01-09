074fba5e5a9ab59c6c6cfd98d4d17c3f8af608b6
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 22:24:40 +0200

    feature: implement add to cart button

---
+++ src/components/AddToCartButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-button
+    :icon="IconCart"
+    type="primary"
+    size="large"
+    :loading
+    @click="$emit('add-to-cart')"
+  >
+    <el-badge :value="1" :offset="[10, 2]">
+      Add to Cart
+    </el-badge>
+  </el-button>
+</template>
+
+<script lang="ts" setup>
+import IconCart from '~icons/icon/cart'
+
+defineProps<{
+  loading: boolean
+}>()
+
+defineEmits(['add-to-cart'])
+</script>
