fb92b45b035dfe14a86ed91f2b4c39c4c2392521
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 22:24:21 +0200

    feature: implement compare button

---
+++ src/components/CompareButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    class="h-[44px] w-[44px] aspect-square"
+    @click="$emit('toggle-compare')"
+  >
+    <IconScales />
+  </button>
+</template>
+
+<script lang="ts" setup>
+import IconScales from '~icons/icon/scales'
+
+defineEmits(['toggle-compare'])
+</script>
