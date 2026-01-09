283277b7f3474a3d1c4e8ed1bda9b3573a7434ad
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 22:28:14 +0200

    feature: add fetching product data method

--- src/views/goods/goods.d.ts
+++ src/views/goods/goods.d.ts
@@ -1,5 +1,23 @@
-interface IToggleFavoriteResponse {
-  success: boolean
+interface IGoodsColorVariant {
+  color: string
+  thumbnail: string
+  images: string[]
+  inStock: boolean
+  availableSizes: IShoeSize[]
+}
+
+interface IGoodsPrice {
+  value: string
+  currency: string
+}
+
+interface IGoodsResponse {
+  productId: number
+  name: string
+  price: IGoodsPrice
+  images: string[]
+  colorVariants: IGoodsColorVariant[]
+  description: string
+  details: string
   isFavorite: boolean
-  productId: string
 }
--- src/views/goods/goods.service.ts
+++ src/views/goods/goods.service.ts
@@ -1,20 +1,36 @@
 class GoodsService {
-  async toggleFavorite (productId: string, currentStatus: boolean): Promise<IToggleFavoriteResponse> {
-    await new Promise(resolve => setTimeout(resolve, 300))
+  async getGoods (): Promise<IGoodsResponse> {
+    await new Promise(resolve => setTimeout(resolve, 500))
+    const response = await fetch('/mocks/goods.json')
+    return response.json()
+  }
+
+  async toggleFavorite (productId: number, currentStatus: boolean) {
+    await new Promise(resolve => setTimeout(resolve, 300, productId))
 
     const newStatus = !currentStatus
 
     return {
       success: true,
-      isFavorite: newStatus,
-      productId
+      isFavorite: newStatus
     }
   }
 
-  async getFavoriteStatus (productId: string): Promise<boolean> {
+  async getFavoriteStatus (productId: number): Promise<boolean> {
     await new Promise(resolve => setTimeout(resolve, 200))
 
-    return productId.startsWith('favorite-')
+    return productId === 123
+  }
+
+  async addToCart (params: {
+    productId: number
+    size: IShoeSize
+    color: string
+  }) {
+    const { productId, size, color } = params
+    await new Promise(resolve => setTimeout(resolve, 300, { productId, size, color }))
+
+    return
   }
 }
 
