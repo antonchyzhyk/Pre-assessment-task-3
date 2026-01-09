534c13bf8eca82ecea48a5f55945e7b08d641a01
Anton Chyzhyk <a.chyzhyk@softonix.org>
2026-01-09 22:25:51 +0200

    feature: implement goods bottom bar component for mobile view

---
+++ src/views/goods/components/GoodsBottomBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sticky bottom-0 px-[16px] py-[24px] w-full bg-white z-10">
+    <div class="flex gap-3">
+      <AddToCartButton
+        class="flex-1"
+        :loading="addToCartLoading"
+        @add-to-cart="$emit('add-to-cart')"
+      />
+
+      <CompareButton @toggle-compare="$emit('toggle-compare')" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  addToCartLoading: boolean
+}>()
+
+defineEmits(['add-to-cart', 'toggle-compare'])
+</script>
